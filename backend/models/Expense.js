39bd0471239c7dd9bf1e5a69aa8c7407ddba4647
sourodip19 <sourodipdey19@gmail.com>
2026-04-01 15:50:54 +0530

    Initialize Node.js backend for expense tracker

---
+++ backend/models/Expense.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose')
+
+const expenseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Expense', expenseSchema)
